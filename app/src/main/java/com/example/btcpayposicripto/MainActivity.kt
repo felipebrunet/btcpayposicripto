9b0b564b30b0493948fd0ccca7e0674c45e3aa87
felipebrunet <felipe.brunet@gmail.com>
2022-12-24 01:51:14 -0300

    primer commit

---
+++ app/src/main/java/com/example/btcpayposicripto/MainActivity.kt
@@ -0,0 +1,123 @@
+package com.example.btcpayposicripto
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_BtcpayPOSIcripto)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val input: TextView = findViewById(R.id.input)
+        val buttonBotondepago: Button = findViewById(R.id.link_twitter)
+        val button1: Button = findViewById(R.id.button_1)
+        val button2: Button = findViewById(R.id.button_2)
+        val button3: Button = findViewById(R.id.button_3)
+        val button4: Button = findViewById(R.id.button_4)
+        val button5: Button = findViewById(R.id.button_5)
+        val button6: Button = findViewById(R.id.button_6)
+        val button7: Button = findViewById(R.id.button_7)
+        val button8: Button = findViewById(R.id.button_8)
+        val button9: Button = findViewById(R.id.button_9)
+        val button0: Button = findViewById(R.id.button_0)
+        val buttonDot: Button = findViewById(R.id.button_dot)
+        val buttonBorrar: Button = findViewById(R.id.button_borrar)
+        val option : Spinner = findViewById(R.id.spinner_currencies)
+        val options = arrayOf("CLP", "USD", "ARS", "EUR", "BRL", "BTC")
+        var moneda = "CLP"
+
+        option.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, options)
+        option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                moneda = options[p2]
+            }
+        }
+
+
+        buttonBorrar.setOnClickListener {
+            input.text = ""
+            input.setTextColor(ContextCompat.getColor(this, R.color.black))
+        }
+        button1.setOnClickListener {
+            input.text = addToInputText("1", input)
+        }
+        button2.setOnClickListener {
+            input.text = addToInputText("2", input)
+        }
+        button3.setOnClickListener {
+            input.text = addToInputText("3", input)
+        }
+        button4.setOnClickListener {
+            input.text = addToInputText("4", input)
+        }
+        button5.setOnClickListener {
+            input.text = addToInputText("5", input)
+        }
+        button6.setOnClickListener {
+            input.text = addToInputText("6", input)
+        }
+        button7.setOnClickListener {
+            input.text = addToInputText("7", input)
+        }
+        button8.setOnClickListener {
+            input.text = addToInputText("8", input)
+        }
+        button9.setOnClickListener {
+            input.text = addToInputText("9", input)
+        }
+
+        button0.setOnClickListener {
+            if (input.text.isEmpty()) {
+                // Show Error Message
+                input.text = addToInputText("", input)
+            }
+            else {
+                input.text = addToInputText("0", input)
+            }
+        }
+
+        buttonDot.setOnClickListener {
+            if (input.text.isEmpty()) {
+                input.text = addToInputText("0.", input)
+            } else {
+                input.text = addToInputText(".", input)
+            }
+        }
+
+        buttonBotondepago.setOnClickListener{
+            if (input.text.isNotEmpty()) {
+                try {
+                    val price: Float = input.text.toString().toFloat()
+                    if (price > 0) {
+                        val urlIcripto = "https://btcpay.icripto.cl/api/v1/invoices?storeId=D8EcMfioGdoiXN9v1ejMth6ZBaVADfsxjocLxbw5h5yH&price=${price}&currency=${moneda}"
+                        startActivity(Intent.parseUri(urlIcripto, 0))
+                    }
+                } catch (e: Exception) {
+                    input.text = "Error"
+                    input.setTextColor(ContextCompat.getColor(this, R.color.red))
+                }
+
+            }
+        }
+    }
+
+    private fun addToInputText(buttonValue: String, input: TextView): String {
+        return "${input.text}$buttonValue"
+    }
+
+}
