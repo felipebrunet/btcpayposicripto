c744d5d8e93be754d59c68743554ce5ae7ffd9e7
felipebrunet <felipe.brunet@gmail.com>
2022-12-28 00:03:06 -0300

    pantalla de ajustes avance 1

---
+++ app/src/main/java/com/example/btcpayposicripto/ActividadAjustes.kt
@@ -0,0 +1,11 @@
+package com.example.btcpayposicripto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActividadAjustes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actividad_ajustes)
+    }
+}
